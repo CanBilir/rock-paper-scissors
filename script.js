6290abba4412df3a5e205ea02387fa966f3803b4
CanBilir <104588030+CanBilir@users.noreply.github.com>
2025-05-03 21:27:22 +0300

    Created functions for getting player's and computer's choice so far it works

--- script.js
+++ script.js
@@ -1,85 +1,28 @@
-let humanScore = 0;
-let computerScore = 0;
-let humanSelection = " ";
-let computerSelection = " ";
+let playerScore = 0;  // value for player score
+let computerScore = 0; // value for computer score
 
-function getComputerChoise() {
-  let num1 = Math.random();
+function getComputerChoice() {      //returns a string that later will be used as computer's choice
+  let num1 = Math.random()
+  let computerChoice = ""
 
-  if(num1 < 0.3) {
-    console.log(num1);
-    return computerSelection = "Rock";
-
+  if(num1 <= 0.3) {
+    computerChoice = "Rock";
   }
 
-  else if(num1 > 0.3 && num1 < 0.6) {
-    console.log(num1);
-    return computerSelection = "Paper";
-  } 
+  else if(num1 > 0.3 && num1 <= 0.6) {
+    computerChoice = "Paper";
+  }
 
   else {
-    console.log(num1);
-    return computerSelection = "Scissors";
+    computerChoice = "Scissors";
   }
+
+  return computerChoice;
+} 
+
+function getHumanChoice() {     //returns a string to be used as player's choice
+  let humanChoice = prompt("Type in your choice!");
+  humanChoice = humanChoice.slice(0,1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+  return humanChoice;
 }
 
-function getHumanChoise() {
-  humanSelection = prompt("Type in your choise!");
-  return humanSelection;
-}
-/*===========================================
-            PLAY ROUND START
-============================================= */
-function playRound(humanChoise, computerChoise) {
-  getComputerChoise();
-  getHumanChoise();
-
-  //Player chooses paper.
-  if(humanChoise.toLowerCase() === "paper" && computerChoise ===  "Rock") {
-    ++humanScore;
-    alert(`You chose Paper and computer chose Rock, you WIN!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-    
-  }
-
-  else if(humanChoise.toLowerCase() === "paper" && computerChoise ===  "Paper") {
-    alert(`It's a Tie!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-
-  else if(humanChoise.toLowerCase() === "paper" && computerChoise ===  "Scissors") {
-    ++computerScore;
-    alert(`You chose Paper and computer chose Scissors, you lose:(!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-  //Player chooses rock.
-  if(humanChoise.toLowerCase() === "rock" && computerChoise ===  "Rock") {
-    alert(`It's a Tie!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-    
-  }
-
-  else if(humanChoise.toLowerCase() === "rock" && computerChoise ===  "Paper") {
-    ++computerScore;
-    alert(`You chose Rock and computer chose Paper, you lose:(\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-
-  else if(humanChoise.toLowerCase() === "rock" && computerChoise ===  "Scissors") {
-    ++humanScore;
-    alert(`You chose Rock and computer chose Scissors, you WIN!:(!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-  //Player Chooses Scissors.
-  if(humanChoise.toLowerCase() === "scissors" && computerChoise ===  "Rock") {
-    ++computerScore;
-    alert(`You chose Scissors and computer chose Rock, you lose:(\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-    
-  }
-
-  else if(humanChoise.toLowerCase() === "scissors" && computerChoise ===  "Paper") {
-    ++humanScore;
-    alert(`You chose Scissors and computer chose Paper, you WIN!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-
-  else if(humanChoise.toLowerCase() === "scissors" && computerChoise ===  "Scissors") {
-    alert(`It's a tie!\nYour Score: ${humanScore} Computer Score: ${computerScore}`);
-  }
-}
-/*===============================================
-                  PLAY ROUND END
-=================================================*/
